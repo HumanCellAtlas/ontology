3d7f3a88d2dc322b737c00d6743c2d7c8784ff0b
Dani Welter <dwelter@ebi.ac.uk>
2019-04-15 15:56:52 +0100

    Initial build errors now fixed

--- src/ontology/get_mirrors.sh
+++ src/ontology/get_mirrors.sh
@@ -4,8 +4,8 @@ mkdir -p mirror
 
 curl https://www.ebi.ac.uk/ols/ontologies/mondo/download > mirror/mondo.owl
 
-#curl https://www.ebi.ac.uk/ols/ontologies/uberon/download > mirror/uberon.owl
+curl https://www.ebi.ac.uk/ols/ontologies/uberon/download > mirror/uberon.owl
 
-curl https://raw.githubusercontent.com/obophenotype/cell-ontology/master/cl-base.owl > mirror/cl.owl
+curl -L https://raw.githubusercontent.com/obophenotype/cell-ontology/master/cl-base.owl > mirror/cl.owl
 
 curl https://raw.githubusercontent.com/EBISPOT/efo/master/src/ontology/efo-edit.owl > mirror/efo-edit.owl
