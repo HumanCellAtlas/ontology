a5ea4c5f3834c13ec6dad199ed5bc299652f73e7
Dani Welter <dwelter@ebi.ac.uk>
2019-04-26 16:52:28 +0100

    Updated get_mirrors script and fixed HCAO-edit to not hardcode HSAPDV.owl version

--- src/ontology/get_mirrors.sh
+++ src/ontology/get_mirrors.sh
@@ -2,11 +2,21 @@
 
 mkdir -p mirror
 
-curl https://www.ebi.ac.uk/ols/ontologies/mondo/download > mirror/mondo.owl
+curl -L https://www.ebi.ac.uk/ols/ontologies/mondo/download > mirror/mondo.owl
 
-curl https://www.ebi.ac.uk/ols/ontologies/uberon/download > mirror/uberon.owl
+curl -L https://www.ebi.ac.uk/ols/ontologies/uberon/download > mirror/uberon.owl
 
-curl -L https://raw.githubusercontent.com/obophenotype/cell-ontology/master/cl.owl > mirror/cl.owl
+curl -L https://www.ebi.ac.uk/ols/ontologies/fma/download > ../imports/fma.owl
 
-curl -L https://github.com/EBISPOT/efo/releases/download/current/efo.owl > mirror/efo.owl
+curl -L https://www.ebi.ac.uk/ols/ontologies/fbbi/download > ../imports/fbbi.owl
+
+curl -L https://www.ebi.ac.uk/ols/ontologies/hsapdv/download > ../imports/hsapdv.owl
+
+curl -L https://www.ebi.ac.uk/ols/ontologies/go/download > ../imports/go-basic.obo
+
+curl -L curl https://www.ebi.ac.uk/ols/ontologies/cl/download > mirror/cl.owl
+
+curl -L https://www.ebi.ac.uk/ols/ontologies/efo/download  > mirror/efo.owl
+
+
 
