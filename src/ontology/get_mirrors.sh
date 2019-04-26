25fa0d5a651c420de31493671bedd94a3ec32b04
Dani Welter <dwelter@ebi.ac.uk>
2019-04-26 14:27:52 +0100

    Make file currently passes but ontology looks weird

--- src/ontology/get_mirrors.sh
+++ src/ontology/get_mirrors.sh
@@ -6,7 +6,7 @@ curl https://www.ebi.ac.uk/ols/ontologies/mondo/download > mirror/mondo.owl
 
 curl https://www.ebi.ac.uk/ols/ontologies/uberon/download > mirror/uberon.owl
 
-curl -L https://raw.githubusercontent.com/obophenotype/cell-ontology/master/cl-base.owl > mirror/cl.owl
+curl -L https://raw.githubusercontent.com/obophenotype/cell-ontology/master/cl.owl > mirror/cl.owl
 
 curl -L https://github.com/EBISPOT/efo/releases/download/current/efo.owl > mirror/efo.owl
 
