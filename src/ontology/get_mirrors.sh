68343f74f2f1042c265fa15981b85cff9a328a1e
Zoe May Pendlington <zmp@ebi.ac.uk>
2021-02-18 16:04:42 +0000

    HCA Ontology release 1.0.20

--- src/ontology/get_mirrors.sh
+++ src/ontology/get_mirrors.sh
@@ -4,21 +4,21 @@ mkdir -p mirror
 
 mkdir -p ../imports
 
-curl -L https://www.ebi.ac.uk/ols/ontologies/mondo/download > mirror/mondo.owl
+curl -L http://purl.obolibrary.org/obo/mondo.obo > mirror/mondo.obo && ../../bin/robot convert -i mirror/mondo.obo -f owl -o mirror/mondo.owl
 
-curl -L https://www.ebi.ac.uk/ols/ontologies/uberon/download > mirror/uberon.owl
+curl -L http://purl.obolibrary.org/obo/uberon.owl > mirror/uberon.owl
 
-curl -L https://www.ebi.ac.uk/ols/ontologies/fma/download > ../imports/fma.owl
+curl -L http://purl.obolibrary.org/obo/fma.owl > ../imports/fma.owl
 
-curl -L https://www.ebi.ac.uk/ols/ontologies/fbbi/download > ../imports/fbbi.owl
+curl -L http://purl.obolibrary.org/obo/fbbi.owl > ../imports/fbbi.owl
 
-curl -L https://www.ebi.ac.uk/ols/ontologies/hsapdv/download > ../imports/hsapdv.owl
+curl -L http://purl.obolibrary.org/obo/hsapdv.owl > ../imports/hsapdv.owl
 
-curl -L https://www.ebi.ac.uk/ols/ontologies/go/download > ../imports/go.owl
+curl -L http://purl.obolibrary.org/obo/go.owl > ../imports/go.owl
 
 curl -L http://purl.obolibrary.org/obo/cl.owl > mirror/cl.owl
 
-curl -L https://www.ebi.ac.uk/ols/ontologies/efo/download  > mirror/efo.owl
+curl -L http://www.ebi.ac.uk/efo/efo.owl  > mirror/efo.owl
 
 curl -L https://www.ebi.ac.uk/ols/ontologies/edam/download  > ../imports/edam.owl
 
