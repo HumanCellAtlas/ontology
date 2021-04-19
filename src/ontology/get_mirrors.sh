02a6b8ea72c0b4c37de3b0b570c327320de573f3
Zoe May Pendlington <zmp@ebi.ac.uk>
2021-04-19 15:22:19 +0100

    HCA ontology release 1.0.22

--- src/ontology/get_mirrors.sh
+++ src/ontology/get_mirrors.sh
@@ -3,24 +3,35 @@
 mkdir -p mirror
 
 mkdir -p ../imports
+echo "DOWNLOADING MONDO..."
+curl -L http://purl.obolibrary.org/obo/mondo.obo > mirror/mondo.obo && ../../bin/robot convert -i mirror/mondo.obo -f owl -o mirror/mondo.owl && echo "MONDO DOWNLOADED"
 
-curl -L http://purl.obolibrary.org/obo/mondo.obo > mirror/mondo.obo && ../../bin/robot convert -i mirror/mondo.obo -f owl -o mirror/mondo.owl
+echo "DOWNLOADING UBERON..."
+curl -L http://purl.obolibrary.org/obo/uberon.owl > mirror/uberon.owl && echo "UBERON DOWNLOADED"
 
-curl -L http://purl.obolibrary.org/obo/uberon.owl > mirror/uberon.owl
+echo "DOWNLOADING FMA..."
+curl -L http://purl.obolibrary.org/obo/fma.owl > ../imports/fma.owl && echo "FMA DOWNLOADED"
 
-curl -L http://purl.obolibrary.org/obo/fma.owl > ../imports/fma.owl
+echo "DOWNLOADING FBBI..."
+curl -L http://purl.obolibrary.org/obo/fbbi.owl > ../imports/fbbi.owl && echo "FBBI DOWNLOADED"
 
-curl -L http://purl.obolibrary.org/obo/fbbi.owl > ../imports/fbbi.owl
+echo "DOWNLOADING HSAPDV..."
+curl -L http://purl.obolibrary.org/obo/hsapdv.owl > ../imports/hsapdv.owl && echo "HSAPDV DOWNLOADED"
 
-curl -L http://purl.obolibrary.org/obo/hsapdv.owl > ../imports/hsapdv.owl
+echo "DOWNLOADING GO..."
+curl -L http://purl.obolibrary.org/obo/go.owl > ../imports/go.owl && echo "GO DOWNLOADED"
 
-curl -L http://purl.obolibrary.org/obo/go.owl > ../imports/go.owl
+echo "DOWNLOADING CL..."
+curl -L http://purl.obolibrary.org/obo/cl.owl > mirror/cl.owl && echo "CL DOWNLOADED"
 
-curl -L http://purl.obolibrary.org/obo/cl.owl > mirror/cl.owl
+echo "DOWNLOADING CLO..."
+curl -L http://purl.obolibrary.org/obo/clo.owl > mirror/clo.owl && echo "CLO DOWNLOADED"
 
-curl -L http://www.ebi.ac.uk/efo/efo.owl  > mirror/efo.owl
+echo "DOWNLOADING EFO..."
+curl -L http://www.ebi.ac.uk/efo/efo.owl  > mirror/efo.owl && echo "EFO DOWNLOADED"
 
-curl -L https://www.ebi.ac.uk/ols/ontologies/edam/download  > ../imports/edam.owl
+echo "DOWNLOADING EDAM..."
+curl -L https://www.ebi.ac.uk/ols/ontologies/edam/download  > ../imports/edam.owl && echo "EDAM DOWNLOADED"
 
 
 
