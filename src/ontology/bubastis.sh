061c43e1aad5496d8267d9197e4084e413749523
bvarner-ebi <95643797+bvarner-ebi@users.noreply.github.com>
2022-09-16 11:39:14 +0100

    HCA ontology release 1.0.43

--- src/ontology/bubastis.sh
+++ src/ontology/bubastis.sh
@@ -1,5 +1,5 @@
 mkdir -p diffs
 java -jar ../../bin/bubastis_1_3.jar -ontology1 "https://raw.githubusercontent.com/ebi-ait/ontology/master/hcao.owl" -ontology2 "hcao.owl" -output ./diffs/hcao_bubastis_diff.txt
 java -jar ../../bin/bubastis_1_3.jar -ontology1 "https://raw.githubusercontent.com/ebi-ait/ontology/master/efo_slim.owl" -ontology2 "efo_slim.owl" -output ./diffs/efo_slim_bubastis_diff.txt
-java -jar ../../bin/bubastis_1_3.jar -ontology1 "https://raw.githubusercontent.com/ebi-ait/ontology/master/fbbi_hcao.owl" -ontology2 "fbbi_hcao.owl" -output ./diffs/fbbi_hcao_diff.txt
+#java -jar ../../bin/bubastis_1_3.jar -ontology1 "https://raw.githubusercontent.com/ebi-ait/ontology/master/fbbi_hcao.owl" -ontology2 "fbbi_hcao.owl" -output ./diffs/fbbi_hcao_diff.txt
 
